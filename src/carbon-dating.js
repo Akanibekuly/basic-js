dc88a1457e90ca6cc138d66543e12c7eb97429b3
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-11 00:56:15 +0600

    feat: carbon-dating ls

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity!='string'){
+    return false;
+  }
+  var reg = new RegExp('^[0-9].+$');
+  console.debug((reg).test(sampleActivity),sampleActivity);
+  if(!(reg).test(sampleActivity)){
+    return false;
+  }
+  var temp=sampleActivity.parseFloat();
+  console.debug(temp);
+  return true;
 };
