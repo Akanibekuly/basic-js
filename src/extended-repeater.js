314cbcc16f7f40a509edcbd05b26b574feb43e78
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-13 21:50:12 +0600

    feat: add solution to extended repeater js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, parameters ) {
+  res='';
+  repeate=1;
+  if(parameters.repeatTimes!=undefined){
+    repeate=parameters.repeatTimes;
+  }
+  separator="+";
+  if (parameters.separator!=null){
+    separator=parameters.separator;
+  }
+  addition='';
+  if(parameters.addition==null&&parameters.additionRepeatTimes!=null){
+    addition+='null';
+  } else if(parameters.addition!=undefined){
+    addition+=parameters.addition;
+  }
+
+  additionRepeate=1;
+  if(parameters.additionRepeatTimes!=undefined){
+    additionRepeate=parameters.additionRepeatTimes;
+  }
+  additionSeparate='';
+  if(parameters.additionSeparator!=undefined){
+    additionSeparate=parameters.additionSeparator;
+  }
+  add='';
+  add+=(addition+additionSeparate).repeat(additionRepeate-1);
+  add+=addition;
+  str+=add;
+  // console.log(str,repeate,separator,addition,additionSeparate,additionRepeate);
+  res=(str+separator).repeat(repeate-1);
+  res+=str;
+  // console.log(res);
+  return res;
 };
   
