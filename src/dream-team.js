ce5e02e55316c808726f8930cd06fbd33c06f7b7
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-13 20:38:47 +0600

    feat: added solution to dream team problem

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,6 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members ) {
   var result='';
-
+  if (members==null){
+    return result;
+  }
+  var res=[];
+  for(var i=0;i<members.length;i++){
+    if(typeof members[i]==='string'){
+      res.push(members[i].trim(" ").charAt(0).toUpperCase());
+    }
+  }
+  res.sort();
+  result=res.join('');
+  // console.log(result);
   return result;
 };
