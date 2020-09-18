c8fa20113321380b8ab9c0b6a2ea09405c08b525
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-19 02:01:12 +0600

    feat: simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  arr: [],
+  getLength(){
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(value==undefined&&value!=null){
+      value='';
+    }
+    this.arr.push(value);
+    // console.log("add ",this.arr,value)
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(position<=0||position>this.getLength()||(typeof position!='number')){
+      this.arr=[];
+      throw "Error: doesn't exist"
+    }
+    var arr1=[];
+    for(var i=0;i<this.getLength();i++){
+      if(i!=position-1){
+        arr1.push(this.arr[i])
+      }
+    }
+    this.arr=arr1;
+    // console.log('remove ',this.arr);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr=this.arr.reverse();
+    // console.log('reverse ',this.arr)
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    var res='';
+    for(var i=0;i<this.getLength();i++){
+      if(i!=0){
+        res+='~~';
+      }
+      
+      res+='( '+this.arr[i]+' )';
+      // console.log(this.arr[i],res);
+    }
+    this.arr=[];
+    return res;
   }
 };
 
