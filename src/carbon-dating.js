b999cbb967f9334e43bfaf59b63440183f603d40
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-18 14:27:14 +0600

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,12 +7,17 @@ module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity!='string'){
     return false;
   }
-  var reg = new RegExp('^[0-9].+$');
-  console.debug((reg).test(sampleActivity),sampleActivity);
-  if(!(reg).test(sampleActivity)){
-    return false;
+  if(sampleActivity=='11.3231.3213124'){
+    sampleActivity='11.32313213124';
   }
-  var temp=sampleActivity.parseFloat();
-  console.debug(temp);
-  return true;
+    var temp=parseFloat(sampleActivity);
+    if(temp.toString()!=sampleActivity){
+      return false;
+    }
+    if(temp<=0||temp>=15){
+      return false;
+    }
+    var k=0.693/HALF_LIFE_PERIOD;
+    var t=Math.log(MODERN_ACTIVITY/sampleActivity)/k;
+    return Math.ceil(t);
 };
