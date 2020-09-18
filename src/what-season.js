9b534ca95cc263dd9048c22f55f55239615f6158
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-18 06:10:23 +0600

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(date==null){
+    return 'Unable to determine the time of year!';
+  }
+  var date1=new Date(date.toString());
+  console.log("Input date is: ",date,"\nImplemented date is: ",date1);
+  if (date1.getMonth()!=date.getMonth()||date1.getFullYear()!=date.getFullYear()){
+    throw new Error('Invalid Date');
+  }
+  month=date1.getMonth();
+  console.log(month);
+  if(month==0||month==1||month==11){
+    return 'winter';
+  } else if(month>=2&&month<=4){
+    return 'spring';
+  } else if(month>=5&&month<=7){
+    return 'summer';
+  } else if(month>=8&&month<=10){
+    return 'autumn'
+  }
+  throw new CustomError('THROWN');
+
 };
